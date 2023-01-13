c28f2bf9767274eef3f7f56554046f80783a7f27
Bezll <jabez1992@hotmail.com>
2023-01-13 21:38:35 +0000

    added calendar, contacts, dashboard, form, invoices and team

--- src/App.js
+++ src/App.js
@@ -4,16 +4,17 @@ import { Routes, Route } from 'react-router-dom';
 import Topbar from "./scenes/global/Topbar";
 import Sidebar from "./scenes/global/Sidebar";
 import Dashboard from "./scenes/dashboard";
-// import Team from "./scenes/team";
-// import Invoices from "./scenes/invoices";
-// import Contacts from "./scenes/contacts";
+import Team from "./scenes/team";
+import Invoices from "./scenes/invoices";
+import Contacts from "./scenes/contacts";
+import Form from "./scenes/form";
+import Calendar from "./scenes/calendar";
 // import Bar from "./scenes/bar";
-// import Form from "./scenes/form";
 // import Line from "./scenes/line";
 // import Pie from "./scenes/pie";
 // import FAQ from "./scenes/faq";
 // import Geography from "./scenes/geography";
-// import Calendar from "./scenes/calendar";
+
 
 
 function App() {
@@ -28,17 +29,18 @@ function App() {
             <main className="content">
               <Topbar />
               <Routes>
-                {/* <Route path="/" element={<Dashboard />} /> */}
-                {/* <Route path="/team" element={<Team />} /> */}
-                {/* <Route path="/invoices" element={<Invoices />} /> */}
-                {/* <Route path="/contacts" element={<Contacts />} /> */}
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/team" element={<Team />} />
+                <Route path="/invoices" element={<Invoices />} />
+                <Route path="/contacts" element={<Contacts />} />
+                <Route path="/form" element={<Form />} />
+                <Route path="/calendar" element={<Calendar />} />
                 {/* <Route path="/bar" element={<Bar />} /> */}
-                {/* <Route path="/form" element={<Form />} /> */}
                 {/* <Route path="/line" element={<Line />} /> */}
                 {/* <Route path="/pie" element={<Pie />} /> */}
                 {/* <Route path="/faq" element={<FAQ />} /> */}
                 {/* <Route path="/geography" element={<Geography />} /> */}
-                {/* <Route path="/calendar" element={<Calendar />} /> */}
+                
               </Routes>
             </main>
           </div>
